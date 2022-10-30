c486c04a2dc61d86c3edfdda440d481469b033d2
Maksim Pliavinski <pliavinski.maksim@gmail.com>
2022-10-31 00:53:07 +0200

    ci: add constants

---
+++ src/constants.tsx
@@ -0,0 +1,8 @@
+export const FIRST_FETCH_LIMIT = 18;
+export const FIRST_FETCH_OFFSET = 0;
+export const NEXT_FETCH_LIMIT = 9;
+export const INITIAL_RECENT_OFFSET = FIRST_FETCH_LIMIT - NEXT_FETCH_LIMIT;
+
+export const IDLE = "idle";
+export const LOADING = "loading";
+export const ERROR = "error";
